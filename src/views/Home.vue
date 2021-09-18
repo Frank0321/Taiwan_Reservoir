624b35da8ef4ab8bf7ed07efff4c69da2a57ee5f
LAPTOP-2HB7CTH5\Frank <lefty21050@gmail.com>
2021-09-18 11:19:47 +0800

    1. 安裝 axios 2. 用axios 讀取數據

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,11 +6,40 @@
 
 <script>
 
-
+import axios from "axios"
+// import axios from "../plugins/axios";
 export default {
   name: 'Home',
   components: {
 
+  },
+  data(){
+    return{
+      rowData:[],
+      DataOne: {},
+
+    }
+  },
+  mounted() {
+    this.fetchData();
+  },
+
+  methods:{
+    fetchData(){
+      // fetch(`https://www.taiwanstat.com/waters/latest`)
+      // .then(function (response) {
+      //   console.log("success");
+      //   console.log("response.json", response.json());
+      //   //有讀出數據了
+      //   console.log(response.data[0]);
+      // })
+
+      axios.get(`https://www.taiwanstat.com/waters/latest`)
+      .then(function (response) {
+        console.log("success");
+        console.log("response", response.data[0]);
+      })
+    }
   }
 }
 </script>
