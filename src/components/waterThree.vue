5e83fe15510dd705de94b8a180c457650a349dce
LAPTOP-2HB7CTH5\Frank <lefty21050@gmail.com>
2021-09-27 22:04:56 +0800

    新建水波 3 的樣式

--- src/components/waterThree.vue
+++ src/components/waterThree.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="boll">
-
+    <div class="test">
+      <p class="num">87%</p>
+    </div>
+    <div class="water w1"></div>
+    <div class="water w2"></div>
+    <div class="water w3"></div>
   </div>
 </template>
 
@@ -12,5 +17,51 @@ export default {
 </script>
 
 <style scoped>
+@-webkit-keyframes move_wave {
+  0% { background-position: 0 0}
+  to { background-position: 532px 0}
+}
+@keyframes move_wave {
+  0% { background-position: 0 0}
+  to { background-position: 532px 0}
+}
+
+.boll{
+  height: 174px;
+  width: 174px;
+  border-radius: 50%;
+  border: 2px solid #fe810c;
+  float: left;
+  position: relative;
+  font-size: 14px;
+  overflow: hidden;
+}
+.test{
+  text-align: center;
+  margin-top: 80%;  /*從這邊改*/
+  color:#fe810c;
+}
+.water {
+  height: 100%;
+  background: url("../assets/image/waves.png") repeat-x;
+  position: absolute;
+  width: 100%;
+  -webkit-animation: move_wave 4s linear infinite;
+  animation: move_wave 4s linear infinite;
+
+}
+.w1 {
+  opacity: .5;
+  background-position: 120px 0;
+  -webkit-animation: move_wave 10s linear infinite;
+  animation: move_wave 10s linear infinite;
+}
+.w2 {
+  opacity: .3;
+  background-position: 60px 0;
+  -webkit-animation: move_wave 8s linear infinite;
+  animation: move_wave 8s linear infinite;
+}
+
 
 </style>
